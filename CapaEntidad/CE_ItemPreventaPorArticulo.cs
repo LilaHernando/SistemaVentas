d0cc750004914b11c63243002599dd6baf6b7d9f
Alejo Aldabe <alejo.aldabe@naldo.com.ar>
2023-10-05 12:04:03 -0300

    Agregue entidades para el modulo de preventa

---
+++ CapaEntidad/CE_ItemPreventaPorArticulo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    class CE_ItemPreventaPorArticulo
+    {
+        public int iden { get; set; }
+        public int GN_Sucursal_iden { get; set; }
+        public int PVTA_Preventa_iden { get; set; }
+        public int GN_Articulo_iden { get; set; }
+        public int PVTA_Preventa_sucursal { get; set; }
+    }
+}
---
+++ CapaEntidad/CE_Preventa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    class CE_Preventa { 
+    
+
+        public int iden { get; set; }
+        public int GN_Sucursal_iden { get; set; }
+        public int GN_Cliente_iden { get; set; }
+        public string fechadecarga { get; set; }
+        public int numero { get; set; }
+        public int monto { get; set; }
+        public int baja { get; set; }
+        public int idOperacion { get; set; }
+    }
+}
+
