8b7115eebe3f34523158233135baf69e5cb72349
Alejo Aldabe <alejo.aldabe@naldo.com.ar>
2023-10-06 09:14:46 -0300

    Hice las entidades

---
+++ CapaEntidad/CE_Remito.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    class CE_Remito
+    {
+        /*Atributos llave*/
+        private int iden {get;set;}
+        private int GN_Sucursal_iden { get; set; }
+        private int CMP_Estado_iden { get; set; }
+        /*---------------*/
+        private int idOperacion { get; set; }
+        private int numero { get; set; }
+        private string tipoRemito { get; set; }
+        private DateTime fechadecarga { get; set; }
+
+        public CE_Remito() { }
+        
+        public CE_Remito(int id,char letra,string tipo,long numero,DateTime fecha,int sucursal, int estado)
+        {
+            this.id = id;
+            this.letra = letra;
+            this.numero = numero;
+            this.fecha = fecha;
+            this.sucursal = sucursal;
+            this.estado = estado;
+        }
+    }
+}
---
+++ CapaEntidad/CE_RemitoPorFactura.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    class CMP_RemitoPorFactura {
+        /*Tabla de llaves*/
+        private int iden { get; set; }
+        private int GN_Sucursal_iden { get; set; }  //Identificador de sucursal
+
+                //Llaves de factura
+        private int CMP_Factura_iden { get; set; }  
+        private int CMP_Factura_sucursal { get; set; }
+
+                //Llaves de remito
+        private int CMP_Remito_iden { get; set; }
+        private int CMP_Remito_sucursal { get; set; }
+
+
+    }
+}
