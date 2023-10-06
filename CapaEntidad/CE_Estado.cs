1db55bb7d2679d13213d5a87b0cd97a6a19ac852
Lila Bergamini <lila.bergamini@naldo.com.ar>
2023-10-06 09:58:47 -0300

    UPDATE Entidades Factura, FacturaPorPreventa, Estado

---
+++ CapaEntidad/CE_Estado.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    class CE_Estado
+    {
+        public int Iden { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
---
+++ CapaEntidad/CE_FacturaPorPreventa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    class CE_FacturaPorPreventa
+    {
+        public int Iden { get; set; }
+        public int GN_Sucursal_iden { get; set; }
+        public int PVTA_Preventa_iden { get; set; }
+        public int PVTA_Preventa_sucursal { get; set; }
+        public int CMP_Factura_iden { get; set; }
+        public int CMP_Factura_sucursal { get; set; }
+
+    }
+   
+}
