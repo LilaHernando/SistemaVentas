789cf766ee232794c5514effd620030db8d4881d
Facundo Figueroa <facundo.figueroa@naldo.com.ar>
2023-10-11 10:21:33 -0300

    Added Sucursal Class

---
+++ CapaEntidad/CE_Sucursal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    class CE_Sucursal
+    {
+        public int Id { get; set; }
+        public int Descripcion { get; set; }
+        public string Telefono { get; set; }
+
+    }
+}
