c66b55babc3b3be394e1cf84071997132e4fa838
Lila Bergamini <lila.bergamini@naldo.com.ar>
2023-10-12 09:11:03 -0300

    ADD Cliente

---
+++ CapaEntidad/CE_Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class CE_Cliente
+    {
+        public int Id { get; set; }
+        public string Dni { get; set; }
+        public String Nombre { get; set; }
+        public String Apellido { get; set; }
+        public DateTime Fecha_nacimiento { get; set; }
+        public string Correo_electronico { get; set; }
+        public string Telefono { get; set; }
+
+    }
+}
